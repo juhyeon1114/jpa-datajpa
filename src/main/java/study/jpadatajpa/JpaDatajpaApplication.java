a4b917ca1f729d5700e3e8e1b8baefcf509f20cd
Kai Kim <juhyeon1114@gmail.com>
2022-12-05 14:35:37 +0900

    init

---
+++ src/main/java/study/jpadatajpa/JpaDatajpaApplication.java
@@ -0,0 +1,13 @@
+package study.jpadatajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaDatajpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaDatajpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/jpadatajpa/JpaDatajpaApplicationTests.java
@@ -0,0 +1,13 @@
+package study.jpadatajpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaDatajpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
